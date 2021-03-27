28f4c2d91cc211ffaa62dd7ca0d1e32bb9cb8be8
Matthew Brett <matthew.brett@gmail.com>
2021-03-27 11:25:53 +0000

    Try fix for Python 3.9

--- config.sh
+++ config.sh
@@ -1,10 +1,20 @@
 # Define custom utilities
-# Test for OSX with [ -n "$IS_OSX" ]
+# Test for OSX with [ -n "$IS_MACOS" ]
 
 function pre_build {
-    # Any stuff that you need to do before you start building the wheels
+    # Any stuff that you need to do before you start
+    # building the wheels.
     # Runs in the root directory of this repository.
-    :
+    # Accelerate error only on macOS
+    if [ -z "$IS_MACOS" ]; then return; fi
+    # Only for Numpy >= 1.20
+    local np_ver=$(python -c 'import numpy; print(numpy.__version__)')
+    if [ $(lex_ver $np_ver) -lt $(lex_ver 1.20) ]; then
+        return
+    fi
+    # Just in case.
+    brew install openblas
+    cp site.cfg.openblas nipy/site.cfg
 }
 
 function run_tests {
